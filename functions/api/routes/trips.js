b3b1e055cb8f8f8916172f9bbd637a51cc48ce8c
Jack McCambridge <jmccambridge06@qub.ac.uk>
2020-08-06 20:56:44 +0100

    Travel itinerary added

--- functions/api/routes/trips.js
+++ functions/api/routes/trips.js
@@ -32,22 +32,30 @@ router.get('/trip/:id', async(req, res, next) => {
     try {
         //const data = await trips.getTrip(userId, tripId);
         const users = await trips.getTripUsers(tripId);
-        const legs = await trips.getTripLegs(tripId);
+        const legs = await trips.getTripLegs(tripId, userId);
+        const activities = await trips.getLegActivities(tripId, userId);
         //res.set('Cache-Control', 'public, max-age=300, s-maxage=600'); // remove caching to a method so it is easily updated and only used when wanted.
-        res.status(200).json({"users": users, "legs": legs});
+        res.status(200).json({"users": users, "legs": legs, "activities": activities});
     } catch (err) {
         next(err);
     }
 });
 
 /**
- * Updates a trips details.
+ * Updates details for an entry.
  */
 router.patch('/trip/:id', async(req, res, next) => {
-    const tripId = parseInt(req.params.id);
+    const id = parseInt(req.params.id);
     const updates = req.body;
+    const type = req.body.type;
     try {
-        const response = await trips.patchTrip(tripId, updates);
+        if (type === 'trip') {
+            await trips.patchTrip(id, updates);
+        } else if (type === 'leg') {
+            await trips.patchLeg(id, updates);
+        } else {
+            await trips.patchActivity(id, updates);
+        }
         //res.set('Cache-Control', 'public, max-age=300, s-maxage=600'); // remove caching to a method so it is easily updated and only used when wanted.
         res.status(200).json({"message": "success"});
     } catch (err) {
@@ -64,7 +72,7 @@ router.post('/trip/:id', async(req, res, next) => {
     const data = req.body;
     try {
         const response = await trips.postTrip(userId, data);
-        data.tripId = response[5][0].ID;
+        data.trip = response[5][0].ID;
         const leg = await trips.postLeg(userId, data);
         console.log(response);
         //res.set('Cache-Control', 'public, max-age=300, s-maxage=600'); // remove caching to a method so it is easily updated and only used when wanted.
@@ -75,13 +83,48 @@ router.post('/trip/:id', async(req, res, next) => {
 });
 
 /**
+ * Creates a new leg and links it to the trip and user.
+ */
+router.post('/leg/:id', async(req, res, next) => {
+    const userId = parseInt(req.params.id);
+    const data = req.body;
+    try {
+        const response = await trips.postLeg(userId, data);
+        console.log(response);
+        //res.set('Cache-Control', 'public, max-age=300, s-maxage=600'); // remove caching to a method so it is easily updated and only used when wanted.
+        res.status(200).json({data: response[5]});
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.post('/activity/:id', async(req, res, next) => {
+    const userId = parseInt(req.params.id);
+    const data = req.body;
+    try {
+        const response = await trips.postActivity(userId, data);
+        console.log(response);
+        //res.set('Cache-Control', 'public, max-age=300, s-maxage=600'); // remove caching to a method so it is easily updated and only used when wanted.
+        res.status(200).json({data: response[5]});
+    } catch (err) {
+        next(err);
+    }
+});
+
+/**
  * Adds a user to a trip
  */
-router.post('/trip/user/:tripId', async(req, res, next) => {
- const tripId = parseInt(req.params.tripId);
- const userId = req.body.user;
+router.post('/trip/user/:id', async(req, res, next) => {
+ const id = parseInt(req.params.id);
+ const values = req.body;
  try {
-    const response = await trips.addTripUser(userId, tripId);
+     if (values.type === 'trip') {
+        await trips.addTripUser(values.user, id);
+     } else if(values.type === 'leg') {
+        await trips.addLegUser(parseInt(values.user), id, parseInt(values.tripId));
+     } else {
+        await trips.addActivityUser(values.user, id, parseInt(values.legId));
+     }
     //res.set('Cache-Control', 'public, max-age=300, s-maxage=600'); // remove caching to a method so it is easily updated and only used when wanted.
     res.status(200).json({"message": "success"});
     } catch (err) {
@@ -90,16 +133,33 @@ router.post('/trip/user/:tripId', async(req, res, next) => {
 });
 
 /**
- * Deletes a user from a trip and subseuqently any activities associated with that trip.
+ * Deletes a user from a an itinerary item (trip, leg or activity) and any other associated items.
+ * If a trip removes from trip, legs and activities. If a leg removes leg and activities. If an activity removes from activity.
  */
-router.delete('/trip/:id', async(req, res, next) => {
+router.delete('/leave/:id', async(req, res, next) => {
     const userId = parseInt(req.params.id);
-    const tripId = req.query.trip;
-    // remove from all activities then remove from trip.
-    const response = await trips.removeTripUser(userId, tripId);
-    //res.json({status: 'success', data: {user}});
-    res.status(200).json({message: "user removed from trip"});
-});
+    const id = parseInt(req.query.id);
+    const type = req.query.type;
+    try {
+        if (type === 'trip') {
+            console.log("about to await trip");
+            await trips.removeTripUser(userId, id);
+        } else if (type === 'leg') {
+            await trips.removeLegUser(userId, id);
+        } else {
+            await trips.removeActivityUser(userId, id);
+        }
+        res.status(200).json({message: "user removed from trip"});
+    } catch(err) {
+        next(err);
+    }
 
+    // remove from all activities then remove from trip.
+    //const removeTrip = await trips.removeTripUser(userId, tripId);
+    //const removeLegs = await trips.removeLegUser(userId, tripId);
+    //await Promise.all([trips.removeTripUser(userId, tripId), trips.removeLegUser(userId, tripId)]);
+    //res.json({status: 'success', data: {user}});
+    
+});
 
 module.exports = router;
--- functions/database/tripsData.js
+++ functions/database/tripsData.js
@@ -24,7 +24,7 @@ function getUsersTrips(userId, status) {
  * @param {*} userId 
  */
 function getTripUsers(tripId) {
-    var sql = "SELECT WT_TripUsers.UserID, Name FROM `WT_TripUsers` INNER JOIN WT_UserProfile ON WT_TripUsers.UserID = WT_UserProfile.UserID \
+    var sql = "SELECT * FROM `WT_TripUsers` INNER JOIN WT_UserProfile ON WT_TripUsers.UserID = WT_UserProfile.UserID \
     WHERE TripID = ?;";
     const parameter = [tripId];
     const errorMessage = "Error retrieving trips users from database."
@@ -35,11 +35,29 @@ function getTripUsers(tripId) {
  * Queries database for a list of legs associated with a trip Id.
  * @param {*} tripId 
  */
-function getTripLegs(tripId) {
+function getTripLegs(tripId, userId) {
     var sql = "SELECT * FROM WT_TripLeg INNER JOIN WT_Leg ON WT_TripLeg.LegID = WT_Leg.ID WHERE TripID = ? \
-                ORDER BY DateStart IS NULL, DateStart;";
-    const parameter = [tripId];
+    AND WT_TripLeg.LegID IN \
+    (Select WT_TripLeg.LegID FROM WT_TripLeg INNER JOIN WT_Leg ON WT_TripLeg.LegID = WT_Leg.ID WHERE WT_TripLeg.TripID = ? \
+    AND WT_TripLeg.UserID = ?) \
+    ORDER BY DateStart IS NULL, DateStart;";
+    const parameter = [tripId, tripId, userId];
     const errorMessage = "Error retrieving trip legs from database."
+    return db.queryDb(sql, parameter, errorMessage);
+}
+
+/**
+ * Queries database for a list of activities associated with a trip id.
+ * @param {*} tripId 
+ */
+function getLegActivities(tripId, userId) {
+    var sql = "SELECT * FROM WT_Activity \
+            INNER JOIN WT_LegActivity ON WT_LegActivity.ActivityID = WT_Activity.ID  \
+            WHERE WT_LegActivity.LegID IN  \
+            (Select WT_TripLeg.LegID FROM WT_TripLeg INNER JOIN WT_Leg ON WT_TripLeg.LegID = WT_Leg.ID WHERE WT_TripLeg.TripID = ?) \
+            ORDER BY DateStart IS NULL, DateStart;";
+    const parameter = [tripId, tripId, userId];
+    const errorMessage = "Error retrieving leg activites from database."
     return db.queryDb(sql, parameter, errorMessage);
 }
 
@@ -50,15 +68,47 @@ function getTripLegs(tripId) {
  */
 function postLeg(userId, updates) {
     const sql = "START TRANSACTION; \
-    INSERT INTO WT_Leg VALUES (NULL, ?, ?, ?, ?, ?); \
+    INSERT INTO WT_Leg VALUES (NULL, ?, ?, ?, ?, ?, ?); \
     SET @last_leg_id = LAST_INSERT_ID(); \
     INSERT INTO WT_TripLeg VALUES (?, @last_leg_id, ?); \
     COMMIT; \
-    SELECT * FROM WT_Leg WHERE ID = @last_leg_id;";
+    SELECT * FROM WT_Leg INNER JOIN WT_TripLeg ON WT_Leg.ID = WT_TripLeg.LegID WHERE ID = @last_leg_id;";
     parameter = [updates.name, db.checkNull(updates.startDate), db.checkNull(updates.finishDate), db.checkNull(updates.description), 
-        db.checkNull(updates.location), userId, updates.tripId];
+        db.checkNull(updates.locationID), db.checkNull(updates.locationDetail), userId, updates.trip];
     const errorMessage = "Error adding leg to database";
     return db.multiqueryDb(sql, parameter, errorMessage);
+}
+
+/**
+ * 
+ * @param {*} userId 
+ * @param {*} updates 
+ */
+function postActivity(userId, updates) {
+    const sql = "START TRANSACTION; \
+    INSERT INTO WT_Activity VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, NULL); \
+    SET @last_activity_id = LAST_INSERT_ID(); \
+    INSERT INTO WT_LegActivity VALUES (?, @last_activity_id, ?); \
+    COMMIT; \
+    SELECT * FROM WT_Activity INNER JOIN WT_LegActivity ON WT_Activity.ID = WT_LegActivity.ActivityID WHERE ID = @last_activity_id;";
+    parameter = [updates.name, db.checkNull(updates.startDate), db.checkNull(updates.finishDate), db.checkNull(updates.description), 
+        db.checkNull(updates.locationID), db.checkNull(updates.locationDetail), db.checkNull(updates.notes), userId, updates.leg];
+    const errorMessage = "Error adding activity to database";
+    return db.multiqueryDb(sql, parameter, errorMessage);
+}
+
+/**
+ * 
+ * @param {*} legId 
+ * @param {*} updates 
+ */
+function patchLeg(legId, updates) {
+    const sql = "UPDATE WT_Leg SET Name = ?, DateStart=?, DateFinish=?, Description=?, LocationID=?, LocationDetail=? \
+     WHERE ID = ?;"
+    parameter = [updates.name, db.checkNull(updates.startDate), db.checkNull(updates.finishDate), db.checkNull(updates.description), 
+        db.checkNull(updates.locationID), db.checkNull(updates.locationDetail), legId];
+    const errorMessage = "Error updating leg in database";
+    return db.queryDb(sql, parameter, errorMessage);
 }
 
 /**
@@ -67,10 +117,10 @@ function postLeg(userId, updates) {
  * @param {*} updates 
  */
 function patchTrip(tripId, updates) {
-    const sql = "UPDATE WT_Trip SET Name = ?, DateStart=?, DateFinish=?, Description=?, Location=?, Picture = ? \
+    const sql = "UPDATE WT_Trip SET Name = ?, DateStart=?, DateFinish=?, Description=?, LocationID=?, LocationDetail=?, Picture = ? \
      WHERE ID = ?;"
     parameter = [updates.name, db.checkNull(updates.startDate), db.checkNull(updates.finishDate), db.checkNull(updates.description), 
-        db.checkNull(updates.location), db.checkNull(updates.picture), tripId];
+        db.checkNull(updates.locationID), db.checkNull(updates.locationDetail), db.checkNull(updates.picture), tripId];
     const errorMessage = "Error updating trip in database";
     return db.queryDb(sql, parameter, errorMessage);
 }
@@ -82,15 +132,29 @@ function patchTrip(tripId, updates) {
  */
 function postTrip(userId, updates) {
     const sql = "START TRANSACTION; \
-    INSERT INTO WT_Trip VALUES (NULL, ?, ?, ?, ?, ?, ?); \
+    INSERT INTO WT_Trip VALUES (NULL, ?, ?, ?, ?, ?, ?, ?); \
     SET @last_trip_id = LAST_INSERT_ID(); \
     INSERT INTO WT_TripUsers VALUES (?, @last_trip_id, 1); \
     COMMIT; \
     SELECT * FROM WT_Trip WHERE ID = @last_trip_id;";
     parameter = [updates.name, db.checkNull(updates.picture), db.checkNull(updates.startDate), db.checkNull(updates.finishDate), db.checkNull(updates.description), 
-        db.checkNull(updates.location), userId];
+        db.checkNull(updates.locationID), db.checkNull(updates.locationDetail), userId];
     const errorMessage = "Error updating trip in database";
     return db.multiqueryDb(sql, parameter, errorMessage);
+}
+
+/**
+ * 
+ * @param {*} activityId 
+ * @param {*} updates 
+ */
+function patchActivity(activityId, updates) {
+    const sql = "UPDATE WT_Activity SET Name = ?, DateStart=?, DateFinish=?, Description=?, LocationID=?, LocationDetail=?, \
+                 Notes = ? WHERE ID = ?;"
+    parameter = [updates.name, db.checkNull(updates.startDate), db.checkNull(updates.finishDate), db.checkNull(updates.description), 
+        db.checkNull(updates.locationID), db.checkNull(updates.locationDetail), db.checkNull(updates.notes), activityId];
+    const errorMessage = "Error updating activity in database";
+    return db.queryDb(sql, parameter, errorMessage);
 }
 
 /**
@@ -106,25 +170,89 @@ function addTripUser(userId, tripId) {
 }
 
 /**
- * Query deletes a user from the trip user table so they are no longer linked to that trip.
+ * 
+ * @param {*} userId 
+ * @param {*} legId 
+ * @param {*} tripId 
+ */
+function addLegUser(userId, legId, tripId) {
+    const sql = "INSERT INTO WT_TripLeg VALUES (?, ?, ?);";
+    parameter = [userId, legId, tripId];
+    const errorMessage = "Error adding user to trip";
+    return db.queryDb(sql, parameter, errorMessage);
+}
+
+/**
+ * 
+ * @param {*} userId 
+ * @param {*} activityId 
+ * @param {*} legIdd 
+ */
+function addActivityUser(userId, activityId, legId) {
+    const sql = "INSERT INTO WT_LegActivity VALUES (?, ?, ?);";
+    parameter = [userId, activityId, legId];
+    const errorMessage = "Error adding user to activity";
+    return db.queryDb(sql, parameter, errorMessage);
+}
+
+/**
+ * Query deletes a user from the trip user table and associated legs and activities so they are no longer linked to that trip.
  * @param {*} userId 
  * @param {*} tripId 
  */
 function removeTripUser(userId, tripId) {
-    const sql = "DELETE FROM WT_TripUsers WHERE UserID = ? AND TripID = ?;";
-    parameter = [userId, tripId];
+    const sql = "START TRANSACTION; \
+                DELETE FROM WT_LegActivity WHERE ActivityUserID = ? AND LegID IN(Select LegID FROM WT_TripLeg WHERE TripID = ?); \
+                DELETE FROM WT_TripLeg WHERE UserID = ? AND TripID = ?; \
+                DELETE FROM WT_TripUsers WHERE UserID = ? AND TripID = ?; \
+                COMMIT;";
+    parameter = [userId, tripId, userId, tripId, userId, tripId];
     const errorMessage = "Error removing user from trip";
-    return db.queryDb(sql, parameter, errorMessage);
+    return db.multiqueryDb(sql, parameter, errorMessage);
 }
 
+/**
+ * Removes user from a leg and any associated activities.
+ * @param {*} userId 
+ * @param {*} tripId 
+ */
+function removeLegUser(userId, legId) {
+    const sql = "START TRANSACTION; \
+                DELETE FROM WT_TripLeg WHERE UserID = ? AND LegID = ?; \
+                DELETE FROM WT_LegActivity WHERE ActivityUserID = ? AND LegID = ?; \
+                COMMIT;";
+    parameter = [userId, legId, userId, legId]; // remove individual leg and all activities.
+    const errorMessage = "Error removing user from trip";
+    return db.multiqueryDb(sql, parameter, errorMessage);
+}
+
+/**
+ * Removes a user from an activity.
+ * @param {*} userId 
+ * @param {*} tripId 
+ */
+function removeActivityUser(userId, activityId) { 
+    const sql = "DELETE FROM WT_LegActivity WHERE ActivityUserID = ? AND ActivityID = ?;";
+    parameter = [userId, activityId];
+    const errorMessage = "Error removing user from activity";
+    return db.queryDb(sql, parameter, errorMessage);
+}
 
 module.exports = {
     getUsersTrips,
     getTripUsers,
     getTripLegs,
+    getLegActivities,
     patchTrip,
+    patchLeg,
+    patchActivity,
     addTripUser,
+    addLegUser,
+    addActivityUser,
     removeTripUser,
+    removeLegUser,
+    removeActivityUser,
     postTrip,
-    postLeg
+    postLeg,
+    postActivity
 }
